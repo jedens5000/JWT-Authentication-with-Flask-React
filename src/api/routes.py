fa6ccd05ea1cdb7ca9c1f2b5b18de571f8aef62e
jedens5000 <mrjonedens@gmail.com>
2022-05-13 19:00:26 +0000

    Login, Private routes added

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,16 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "super-secret"
+
+jwt = JWTManager(app)
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +26,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+     username = request.json.get("username", None)
+     password = request.json.get("password", None)
+     print(username, password)
+     #return jsonify({username:username})
+     secret_key = "heytherebb"
+     #access_token = create_access_token(identity=username)
+     return jsonify(access_token=secret_key)
+
+@api.route('/private', methods=['POST'])
+def getprivate():
+    secret_key = "heytherebb"
+    Authorization = request.headers.get("Authorization")
+    if Authorization == "":
+        return jsonify("you are not logged in")
+    elif Authorization == secret_key:
+        return jsonify("you are logged in")  
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "username": user.username }), 200
