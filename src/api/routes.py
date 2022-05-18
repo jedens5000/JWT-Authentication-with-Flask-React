afea6022952f7b289b8c331fddb4a2c67f27ca29
jedens5000 <mrjonedens@gmail.com>
2022-05-18 21:07:51 +0000

    complete

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,11 +20,9 @@ api = Blueprint('api', __name__)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
 
 @api.route('/login', methods=['POST'])
@@ -32,25 +30,15 @@ def login():
      username = request.json.get("username", None)
      password = request.json.get("password", None)
      print(username, password)
-     #return jsonify({username:username})
-     secret_key = "heytherebb"
+     secret_key = "OU812"
      #access_token = create_access_token(identity=username)
      return jsonify(access_token=secret_key)
 
 @api.route('/private', methods=['POST'])
 def getprivate():
-    secret_key = "heytherebb"
+    secret_key = "OU812"
     Authorization = request.headers.get("Authorization")
     if Authorization == "":
         return jsonify("you are not logged in")
     elif Authorization == secret_key:
         return jsonify("you are logged in")  
-
-@app.route("/protected", methods=["GET"])
-@jwt_required()
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user_id = get_jwt_identity()
-    user = User.query.get(current_user_id)
-    
-    return jsonify({"id": user.id, "username": user.username }), 200
